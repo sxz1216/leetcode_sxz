3d2576aeb9bdcefcdeb79efc8ec653e4f464c27e
xuanzhi <729977187@qq.com>
2018-07-03 15:08:22 +0800

    双指针

---
+++ 141_环形链表.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None:
+            return False
+        fast = head
+        slow = head
+        while fast.next != None:
+            fast = fast.next.next
+            if fast == None:
+                return False
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
