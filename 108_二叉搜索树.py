b0c9a57499ffb2f02ca2afde9becae7ce3e073b4
xuanzhi <729977187@qq.com>
2018-07-10 16:46:47 +0800

    //_and_

---
+++ 108_二叉搜索树.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        size = len(nums)
+        if size == 0:
+            return None
+        if size == 1:
+            return TreeNode(nums[0])
+        size //= 2
+        root = TreeNode(nums[size])
+        root.left = self.sortedArrayToBST(nums[:size])
+        root.right = self.sortedArrayToBST(nums[size + 1:])
+        return root
