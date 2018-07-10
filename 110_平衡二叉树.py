8367d1cbc35a3335bfafc4b0fd85c587b801078e
xuanzhi <729977187@qq.com>
2018-07-10 17:38:55 +0800

    recursive

---
+++ 110_平衡二叉树.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True 
+        if abs(self.height(root.left) - self.height(root.right)) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
+    def height(self,r):
+        if r == None:
+            return 0
+        return max(self.height(r.left),self.height(r.right)) + 1
